1f53e5756dcc48dbf697072c95f9df526a5520f7
Hussain Mahamud <hussainmahamud.swe@gmail.com>
2023-08-18 16:26:45 +0600

    Changed Package type project to library

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd831c2bad9545ea5fe0a15e05f7e0d5e6f25b2a5',
+        'name' => 'hussain/post',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hussain/post' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd831c2bad9545ea5fe0a15e05f7e0d5e6f25b2a5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
